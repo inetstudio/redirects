b12991af8ea667645c48bfa9c8dbff84ba2e2b96
Konstantin Bukin <bukin@inetstudio.ru>
2021-12-08 11:19:14 +0300

    Update

--- entities/redirects/resources/views/back/includes/navigation.blade.php
+++ entities/redirects/resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.redirects-package.redirects.*') }}">
+<li class="{{ isActiveRoute('back.redirects-package.redirects.*', 'mm-active') }}">
     <a href="{{ route('back.redirects-package.redirects.index') }}"><i class="fa fa-random"></i> <span class="nav-label">Редиректы</span></a>
 </li>
